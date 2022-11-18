d9cbacc55d6a1c90436a84a6405355671c451d24
adeleyepaul <adeleyepaul>
2022-11-18 08:07:12 +0100

    delete module

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,3 +27,22 @@ $("#update_todo").submit(function(event){
         alert("Data Updated Successfully")
     })
 })
+
+if(window.location.pathname=="/"){
+    $ondelete = $(".table tbody td a.delete")
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request={
+            "url": `http://localhost:4500/api/todo/${id}`,
+            "method": "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully")
+                location.reload()
+            })   
+        }
+    })
+}
